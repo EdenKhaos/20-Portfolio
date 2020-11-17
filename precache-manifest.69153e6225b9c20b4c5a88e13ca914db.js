eb8200f9c1c136e6cc4b38980e6c51e3676afb2d
Heather <raven.dantes@ymail.com>
2020-11-16 18:03:42 -0700

    Updates

---
+++ precache-manifest.69153e6225b9c20b4c5a88e13ca914db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d69de006a1f7d813070f632badb587",
+    "url": "/20-Portfolio/index.html"
+  },
+  {
+    "revision": "4998bc073e13342a9b7b",
+    "url": "/20-Portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "4d07702d38d670bbdbf1",
+    "url": "/20-Portfolio/static/js/2.d4997fa8.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/20-Portfolio/static/js/2.d4997fa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4998bc073e13342a9b7b",
+    "url": "/20-Portfolio/static/js/main.6116646e.chunk.js"
+  },
+  {
+    "revision": "022ead6d44eb42ca9c59",
+    "url": "/20-Portfolio/static/js/runtime-main.5858f504.js"
+  }
+]);
